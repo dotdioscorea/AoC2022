bed0c22774953809655e642c8ceee7e96f877756
dotdioscorea <github@awr.sh>
2022-12-12 19:39:45 +0000

    Day 12 challenge solution

---
+++ Day12/main.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+struct position { int x; int y; };
+
+position neighbors[4] {position{-1, 0},
+                       position{1, 0},
+                       position{0, 1},
+                       position{0, -1}};
+
+void readInput (char **heightMap, int **nodeMap, std::vector<position> &newNodes, position &endPoint, int &width, int &height);
+bool dijkstraCycle (char **heightMap, int **nodeMap, std::vector<position> &newNodes, position &endPoint, int &width, int &height, int &shortestPath, int direction);
+void printCurrentState(char **heightMap, int **nodeMap);
+
+int main() {
+    //READ AND PARSE INPUT FILE
+    int width = 179;
+    int height = 41;
+    char **heightMap = (char **) calloc(width, sizeof(char *));
+    int **nodeMap = (int **) calloc(width, sizeof(int *));
+    for (int i = 0; i < width; i++) {
+        heightMap[i] = (char *) calloc(height, sizeof(char));
+        nodeMap[i] = (int *) calloc(height, sizeof(int));
+    }
+    position endPoint;
+    std::vector<position> newNodes;
+    int shortestPath;
+
+
+    readInput(heightMap, nodeMap, newNodes, endPoint, width, height);
+    while(true)
+        if(dijkstraCycle(heightMap, nodeMap, newNodes, endPoint, width, height, shortestPath, 0))
+            break;
+    std::cout << "Found route with length " << shortestPath << std::endl;
+
+    //    std::memset(nodeMap, -1, sizeof nodeMap);
+    for (int y = height - 1; y >= 0; y--)
+        for (int x = 0; x < width; x++)
+            nodeMap[x][y] = -1;
+    nodeMap[endPoint.x][endPoint.y] = 0;
+    newNodes.clear();
+    newNodes.push_back(position{endPoint.x, endPoint.y});
+    while(true)
+        if(dijkstraCycle(heightMap, nodeMap, newNodes, endPoint, width, height, shortestPath, 1))
+            break;
+
+    std::cout << "Shortest A route is " << shortestPath - 1;
+
+    return 0;
+}
+
+bool dijkstraCycle(char **heightMap, int **nodeMap, std::vector<position> &newNodes, position &endPoint, int &width, int &height, int &shortestPath, int direction) {
+    std::vector<position> newnewNodes;
+
+    for (position p: newNodes) {
+        //CHECK NEIGHBORS
+        for (position n: neighbors) {
+            //OUT OF BOUNDS CASE
+            if (p.x + n.x < 0 ||
+                p.x + n.x > width - 1 ||
+                p.y + n.y < 0 ||
+                p.y + n.y > height - 1)
+                continue;
+
+            //CHECK NODE NEIGHBOUR ELIGIBILITY
+            if (nodeMap[p.x + n.x][p.y + n.y] != -1)
+                continue;
+
+            //CHECK IF SAME LETTER OR +1
+            if (direction == 0)
+                if (heightMap[p.x + n.x][p.y + n.y] - heightMap[p.x][p.y] > 1)
+                    continue;
+            if (direction == 1)
+                if (heightMap[p.x][p.y] - heightMap[p.x + n.x][p.y + n.y] > 1)
+                    continue;
+
+            //NODE ELIGIBLE
+            newnewNodes.push_back(position{p.x + n.x, p.y + n.y});
+
+            //GET SMALLEST VALUE AVAILABLE AND SET
+            int value = nodeMap[p.x][p.y] + 1;
+            for (position n2: neighbors) {
+                //OUT OF BOUNDS CASE PREVENTION
+                if (p.x + n.x + n2.x < 0 ||
+                    p.x + n.x + n2.x > width - 1 ||
+                    p.y + n.y + n2.y < 0 ||
+                    p.y + n.y + n2.y > height - 1)
+                    continue;
+
+                //CHECK IF SAME LETTER OR +1
+                if (direction == 0)
+                    if (heightMap[p.x + n.x][p.y + n.y] - heightMap[p.x + n.x + n2.x][p.y + n.y + n2.y] > 1)
+                        continue;
+                if (direction == 1)
+                    if (heightMap[p.x + n.x + n2.x][p.y + n.y + n2.y] - heightMap[p.x + n.x][p.y + n.y] > 1)
+                        continue;
+
+                //CHECK IF NEIGHBOUR NODE'S NEIGHBOURS ARE UNSET, AND FOR LOWER VALUE
+                if (nodeMap[p.x + n.x + n2.x][p.y + n.y + n2.y] != -1 &&
+                    nodeMap[p.x + n.x + n2.x][p.y + n.y + n2.y] + 1 < value)
+                    value = nodeMap[p.x + n.x + n2.x][p.y + n.y + n2.y] + 1;
+            }
+            nodeMap[p.x + n.x][p.y + n.y] = value;
+
+            //HANDLE FINISHED CASE
+            if (direction == 0)
+                if (p.x + n.x == endPoint.x && p.y + n.y == endPoint.y) {
+                    shortestPath = value;
+                    return true;
+                }
+            if (direction == 1)
+                if (heightMap[p.x][p.y] == 'a') {
+                    shortestPath = value;
+                    return true;
+                }
+        }
+    }
+    newNodes = newnewNodes;
+    if (direction == 1)
+        printCurrentState(heightMap, nodeMap);
+    return false;
+}
+
+void printCurrentState(char **heightMap, int **nodeMap) {
+    for (int y = 40; y >= 0; y--) {
+        for (int x = 0; x < 179; x++)
+            std::cout << nodeMap[x][y];
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void readInput (char **heightMap, int **nodeMap, std::vector<position> &newNodes, position &endPoint, int &width, int &height) {
+    std::fstream input("Day12/input.txt");
+    if (!input.is_open())
+        return;
+    std::string line;
+    std::getline(input, line);
+
+    width = line.size();
+
+    //POPULATE ARRAYS
+    int x = 0, y = height - 1;
+    while (y >= 0) {
+        for (char c: line) {
+            //FIND START POS
+            if (c == 'S') {
+                c = 'a';
+                newNodes.push_back(position{x, y});
+                nodeMap[x][y] = 0;
+            }
+            else
+                nodeMap[x][y] = -1;
+
+            //FIND END POS
+            if (c == 'E') {
+                endPoint = position{x, y};
+                c = 'z';
+            }
+
+            heightMap[x][y] = c;
+            x++;
+        }
+        y --;
+        x = 0;
+        std::getline(input, line);
+    }
+    return;
+}
