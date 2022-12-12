8cc74ebba19b7e12f53504d082f022bf79ebfeb7
dotdioscorea <github@awr.sh>
2022-12-12 19:49:24 +0000

    Day 12 challenge solution (tidied up)

--- Day12/main.cpp
+++ Day12/main.cpp
@@ -9,14 +9,17 @@ position neighbors[4] {position{-1, 0},
                        position{0, 1},
                        position{0, -1}};
 
+void getDimensions(int &width, int &height);
 void readInput (char **heightMap, int **nodeMap, std::vector<position> &newNodes, position &endPoint, int &width, int &height);
 bool dijkstraCycle (char **heightMap, int **nodeMap, std::vector<position> &newNodes, position &endPoint, int &width, int &height, int &shortestPath, int direction);
-void printCurrentState(char **heightMap, int **nodeMap);
 
 int main() {
-    //READ AND PARSE INPUT FILE
+    //GET DIMENSIONS OF MAP
     int width = 179;
     int height = 41;
+    getDimensions(width, height);
+
+    //POPULATE ARRAYS
     char **heightMap = (char **) calloc(width, sizeof(char *));
     int **nodeMap = (int **) calloc(width, sizeof(int *));
     for (int i = 0; i < width; i++) {
@@ -26,25 +29,27 @@ int main() {
     position endPoint;
     std::vector<position> newNodes;
     int shortestPath;
-
-
     readInput(heightMap, nodeMap, newNodes, endPoint, width, height);
+
+    //PERFORM PART 1 ALGORITHM
     while(true)
         if(dijkstraCycle(heightMap, nodeMap, newNodes, endPoint, width, height, shortestPath, 0))
             break;
     std::cout << "Found route with length " << shortestPath << std::endl;
 
-    //    std::memset(nodeMap, -1, sizeof nodeMap);
+    //RESET NODE ARRAY (DUNNO WHY MEMSET NOT WORKING HERE)
     for (int y = height - 1; y >= 0; y--)
         for (int x = 0; x < width; x++)
             nodeMap[x][y] = -1;
+    //SET START POINT TO PREVIOUS END POINT
     nodeMap[endPoint.x][endPoint.y] = 0;
     newNodes.clear();
     newNodes.push_back(position{endPoint.x, endPoint.y});
+
+    //REPEAT ALGORITHM WORKING BACKWARDS FROM START
     while(true)
         if(dijkstraCycle(heightMap, nodeMap, newNodes, endPoint, width, height, shortestPath, 1))
             break;
-
     std::cout << "Shortest A route is " << shortestPath - 1;
 
     return 0;
@@ -117,28 +122,30 @@ bool dijkstraCycle(char **heightMap, int **nodeMap, std::vector<position> &newNo
         }
     }
     newNodes = newnewNodes;
-    if (direction == 1)
-        printCurrentState(heightMap, nodeMap);
     return false;
 }
 
-void printCurrentState(char **heightMap, int **nodeMap) {
-    for (int y = 40; y >= 0; y--) {
-        for (int x = 0; x < 179; x++)
-            std::cout << nodeMap[x][y];
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
+void getDimensions(int &width, int &height) {
+    //PREPARE VARIABLES
+    std::fstream input("Day12/input.txt");
+    if (!input.is_open())
+        return;
+    std::string line;
+
+    //SETUP DIMENSIONS
+    width = line.size();
+    height = 0;
+    //FIRST PASS RUN TO GET HEIGHT OF MAP
+    while (std::getline(input, line)) height++;
 }
 
 void readInput (char **heightMap, int **nodeMap, std::vector<position> &newNodes, position &endPoint, int &width, int &height) {
+    //PREPARE VARIABLES
     std::fstream input("Day12/input.txt");
     if (!input.is_open())
         return;
     std::string line;
     std::getline(input, line);
-
-    width = line.size();
 
     //POPULATE ARRAYS
     int x = 0, y = height - 1;
@@ -146,16 +153,19 @@ void readInput (char **heightMap, int **nodeMap, std::vector<position> &newNodes
         for (char c: line) {
             //FIND START POS
             if (c == 'S') {
+                //SET START VALUE TO 'Z' SO ALGORITHM WORKS
                 c = 'a';
                 newNodes.push_back(position{x, y});
+                //START POS HAS NODE VALUE 0
                 nodeMap[x][y] = 0;
             }
             else
-                nodeMap[x][y] = -1;
+                nodeMap[x][y] = -1; //UNCHECKED NODES ARE -1
 
             //FIND END POS
             if (c == 'E') {
                 endPoint = position{x, y};
+                //SET END VALUE TO 'Z' SO ALGORITHM WORKS
                 c = 'z';
             }
 
